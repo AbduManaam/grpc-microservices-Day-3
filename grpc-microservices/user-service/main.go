25389c1f3640e8a3922196af82154592973864aa
Abdu Manaam <abdumanaam01@gmail.com>
2026-04-22 21:02:35 +0530

    added all

---
+++ grpc-microservices/user-service/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net"
+	    "google.golang.org/grpc"
+
+	userpb "user-service/gen/user"
+)
+
+func main(){
+
+   srv:=grpc.NewServer()
+   
+   userpb.RegisterUserServiceServer(srv,NewUserServer())
+   
+   lis,err:= net.Listen("tcp",":50051")  //Just opens/creates the port — but no requests accepted yet
+
+   if err!=nil{
+	log.Fatalf("Listen: %v",err)
+   }
+
+   log.Println("Userservice listening on :50051")
+   if err:=srv.Serve(lis);err!=nil{   //Hands the port to gRPC server — NOW requests start coming in
+	log.Fatalf("serve: %v",err)
+   }
+
+}
